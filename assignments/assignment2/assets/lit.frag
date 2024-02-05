5f8bb9a0d7f5c52fbee9ca6196e010ddc76924ce
Adam Gyenes <adam.gyenes03@gmail.com>
2024-02-05 10:30:37 -0500

    Created assignment2 (fork of assignment1)

---
+++ assignments/assignment2/assets/lit.frag
@@ -0,0 +1,47 @@
+#version 450
+
+in Surface
+{
+	vec3 pos;
+	mat3 tbn;
+	vec2 UV;
+} fs_in;
+
+struct Material
+{
+	float ambientStrength;
+	float diffuseStrength;
+	float specularStrength;
+	float shininess;
+};
+
+uniform vec3 _cameraPosition;
+uniform vec3 _lightDirection = vec3(0.0, -1.0, 0.0);
+uniform vec3 _ambientColor;
+uniform vec3 _lightColor;
+uniform sampler2D _mainTex;
+uniform sampler2D _normalTex;
+uniform Material _material;
+
+out vec4 FragColor;
+
+void main()
+{
+	//Light calculations
+	vec3 normal = texture(_normalTex, fs_in.UV).rgb;
+	normal = normalize(normal * 2.0 - 1.0);
+	vec3 toLight =  fs_in.tbn * -_lightDirection;
+	vec3 toCamera = fs_in.tbn * normalize(_cameraPosition - fs_in.pos);
+	float diffuseFactor = max(dot(normal, toLight), 0.0);
+	
+	//Specular reflection
+	vec3 h = normalize(toLight + toCamera);
+	float specularFactor = pow(max(dot(normal, h), 0.0), _material.shininess);
+
+	vec3 light = _ambientColor * _material.ambientStrength;
+	light += _lightColor * diffuseFactor * _material.diffuseStrength;
+	light += _lightColor * specularFactor * _material.specularStrength;
+	vec3 object = texture(_mainTex, fs_in.UV).rgb;
+
+	FragColor = vec4(object * light, 1.0);
+}
---
+++ assignments/assignment2/main.cpp
@@ -0,0 +1,317 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+#include <ew/transform.h>
+#include <ew/texture.h>
+
+#include <util/model.h>
+#include <util/framebuffer.h>
+#include <util/shader.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
+
+//Global state
+constexpr glm::vec3 CAMERA_INIT_POSITION = glm::vec3(0.f, 0.f, 5.f);
+constexpr glm::vec3 CAMERA_INIT_TARGET = glm::vec3(0.f);
+
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
+
+ew::Camera camera;
+ew::CameraController cameraController;
+float cameraFov = 60.f;
+
+struct Material
+{
+	float ambientStrength = 1.0;
+	float diffuseStrength = 0.5;
+	float specularStrength = 0.5;
+	float shininess = 128;
+};
+
+Material material;
+glm::vec3 lightColor = glm::vec3(1.0);
+glm::vec3 ambientColor = glm::vec3(0.3, 0.4, 0.45);
+
+GLuint currentColorTexture;
+GLuint currentNormalTexture;
+int currentTexture = 0;
+int prevTexture = 0;
+GLuint brickColorTexture;
+GLuint brickNormalTexture;
+GLuint rockColorTexture;
+GLuint rockNormalTexture;
+
+Util::Framebuffer postprocessFramebuffer;
+bool boxBlurEnable = false;
+int boxBlurSize = 2;
+
+bool chromaticAberrationEnable = false;
+float chromaticAberrationSize = 0.09;
+glm::vec2 focusPoint(0.5f, 0.5f);
+
+bool dofEnable = false;
+int dofBlurSize = 8;
+float dofMinDistance = 1.0;
+float dofMaxDistance = 3.0;
+
+void resetCamera(ew::Camera* camera, ew::CameraController* controller)
+{
+	cameraFov = 60.f;
+	camera->position = CAMERA_INIT_POSITION;
+	camera->target = CAMERA_INIT_TARGET;
+	camera->fov = cameraFov;
+	controller->yaw = 0.f;
+	controller->pitch = 0.f;
+}
+
+void createPostprocessFramebuffer(int width, int height)
+{
+	postprocessFramebuffer = Util::Framebuffer(glm::vec2(width, height));
+	postprocessFramebuffer.addColorAttachment();
+	postprocessFramebuffer.addDepthAttachment();
+}
+
+void startRenderSceneToFramebuffer(const Util::Framebuffer& framebuffer)
+{
+	if (!framebuffer.isComplete())
+	{
+		printf("Attempt to render to an incomplete framebuffer\n");
+		return;
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.getFBO());
+	glViewport(0, 0, framebuffer.getSize().x, framebuffer.getSize().y);
+	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+int main() {
+	GLFWwindow* window = initWindow("Assignment 2", screenWidth, screenHeight);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	createPostprocessFramebuffer(screenWidth, screenHeight);
+
+	camera.position = CAMERA_INIT_POSITION;
+	camera.target = CAMERA_INIT_TARGET;
+	camera.aspectRatio = float(screenWidth) / float(screenHeight);
+	camera.fov = cameraFov;
+
+	Util::Shader shader("assets/lit.vert", "assets/lit.frag");
+	Util::Shader postprocessShader("assets/postprocess.vert", "assets/postprocess.frag");
+	Util::Model monkeyModel("assets/Suzanne.obj");
+	ew::Transform monkeyTransform;
+
+	brickColorTexture = ew::loadTexture("assets/brick2_color.jpg");
+	brickNormalTexture = ew::loadTexture("assets/brick2_normal.jpg");
+
+	rockColorTexture = ew::loadTexture("assets/rock_color.jpg");
+	rockNormalTexture = ew::loadTexture("assets/rock_normal.jpg");
+
+	currentColorTexture = brickColorTexture;
+	currentNormalTexture = brickNormalTexture;
+
+	//Create dummy VAO for screen
+	GLuint screenVAO;
+	glCreateVertexArrays(1, &screenVAO);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - prevFrameTime;
+		prevFrameTime = time;
+
+		//Start rendering to offscreen fbo
+		startRenderSceneToFramebuffer(postprocessFramebuffer);
+
+		cameraController.move(window, &camera, deltaTime);
+		camera.fov = cameraFov;
+
+		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.f, 1.f, 0.f));
+
+		glBindTextureUnit(0, currentColorTexture);
+		glBindTextureUnit(1, currentNormalTexture);
+
+		shader.use();
+		shader.setMat4("_model", monkeyTransform.modelMatrix());
+		shader.setMat4("_viewProjection", camera.projectionMatrix() * camera.viewMatrix());
+		shader.setVec3("_cameraPosition", camera.position);
+		shader.setInt("_mainTex", 0);
+		shader.setInt("_normalTex", 1);
+		shader.setVec3("_ambientColor", ambientColor);
+		shader.setVec3("_lightColor", lightColor);
+		shader.setFloat("_material.ambientStrength", material.ambientStrength);
+		shader.setFloat("_material.diffuseStrength", material.diffuseStrength);
+		shader.setFloat("_material.specularStrength", material.specularStrength);
+		shader.setFloat("_material.shininess", material.shininess);
+
+		monkeyModel.draw();
+
+		//Render to screen (default) fbo
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glBindTextureUnit(0, postprocessFramebuffer.getColorAttachment());
+		glBindTextureUnit(1, postprocessFramebuffer.getDepthAttachment());
+		postprocessShader.use();
+
+		postprocessShader.setSubroutine(GL_FRAGMENT_SHADER, {
+			std::make_pair("_blurFunction", boxBlurEnable ? "blurEnabled" : "blurDisabled"),
+			std::make_pair("_dofFunction", dofEnable ? "dofEnabled" : "dofDisabled"),
+			std::make_pair("_chromaticAberrationFunction", chromaticAberrationEnable ? "chromaticAberrationEnabled" : "chromaticAberrationDisabled") });
+
+		postprocessShader.setInt("_colorBuffer", 0);
+		postprocessShader.setInt("_depthBuffer", 1);
+		postprocessShader.setVec2("_focusPoint", focusPoint);
+		postprocessShader.setInt("_boxBlurSize", boxBlurSize);
+		postprocessShader.setFloat("_chromaticAberrationSize", chromaticAberrationSize);
+		postprocessShader.setFloat("_dofMinDistance", dofMinDistance);
+		postprocessShader.setFloat("_dofMaxDistance", dofMaxDistance);
+		postprocessShader.setInt("_dofBlurSize", dofBlurSize);
+
+		glBindVertexArray(screenVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		drawUI();
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+void drawUI() {
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Settings");
+	if (ImGui::CollapsingHeader("Light"))
+	{
+		ImGui::ColorEdit3("Ambient color", &ambientColor[0], ImGuiColorEditFlags_Float);
+		ImGui::ColorEdit3("Light color", &lightColor[0], ImGuiColorEditFlags_Float);
+	}
+	if (ImGui::CollapsingHeader("Material"))
+	{
+		ImGui::Combo("Texture", &currentTexture, "Brick\0Rock");
+		if (currentTexture != prevTexture)
+		{
+			prevTexture = currentTexture;
+			if (currentTexture == 0)
+			{
+				currentColorTexture = brickColorTexture;
+				currentNormalTexture = brickNormalTexture;
+			}
+			else if (currentTexture == 1)
+			{
+				currentColorTexture = rockColorTexture;
+				currentNormalTexture = rockNormalTexture;
+			}
+		}
+
+		ImGui::SliderFloat("Ambient strength", &material.ambientStrength, 0.f, 1.f);
+		ImGui::SliderFloat("Diffuse strength", &material.diffuseStrength, 0.f, 1.f);
+		ImGui::SliderFloat("Specular strength", &material.specularStrength, 0.f, 1.f);
+		ImGui::DragFloat("Shininess", &material.shininess, 1.f, 0.f, 1024.f);
+	}
+	if (ImGui::CollapsingHeader("Camera"))
+	{
+		ImGui::SliderFloat("FOV", &cameraFov, 20.f, 180.f);
+		if (ImGui::Button("Reset"))
+		{
+			resetCamera(&camera, &cameraController);
+		}
+	}
+	if (ImGui::CollapsingHeader("Post processing"))
+	{
+		ImGui::SliderFloat2("Focus point", &focusPoint[0], 0.0f, 1.f);
+
+		ImGui::Indent();
+		if (ImGui::CollapsingHeader("Box blur"))
+		{
+			ImGui::Checkbox("Enable##1", &boxBlurEnable);
+			ImGui::DragInt("Size##1", &boxBlurSize, 0.1f, 0, 99);
+		}
+		if (ImGui::CollapsingHeader("Depth of field"))
+		{
+			ImGui::Checkbox("Enable##2", &dofEnable);
+			ImGui::DragInt("Blur size", &dofBlurSize, 0.1f, 1, 99);
+			ImGui::DragFloat("Min distance", &dofMinDistance, 0.1f);
+			ImGui::DragFloat("Max distance", &dofMaxDistance, 0.1f);
+		}
+		if (ImGui::CollapsingHeader("Chromatic aberration"))
+		{
+			ImGui::Checkbox("Enable##3", &chromaticAberrationEnable);
+			ImGui::SliderFloat("Size##2", &chromaticAberrationSize, 0.0f, 1.f);
+		}
+		ImGui::Unindent();
+	}
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+
+	//Generate new framebuffer with updated size
+	createPostprocessFramebuffer(screenWidth, screenHeight);
+}
+
+/// <summary>
+/// Initializes GLFW, GLAD, and IMGUI
+/// </summary>
+/// <param name="title">Window title</param>
+/// <param name="width">Window width</param>
+/// <param name="height">Window height</param>
+/// <returns>Returns window handle on success or null on fail</returns>
+GLFWwindow* initWindow(const char* title, int width, int height) {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return nullptr;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	return window;
+}
+
